d1a02c5ef05e256c43de851d42d9e6a32f0daf82
Loïc Fejoz <loic@fejoz.net>
2014-09-30 14:58:22 +0200

    Add License header to test4.lua

--- src/test4.lua
+++ src/test4.lua
@@ -1,3 +1,7 @@
+-- Copyright (c) 2014 Loïc Fejoz
+-- This file is provided under the MIT License.
+-- author(s):
+--  * Loïc Fejoz <loic@fejoz.net>
 for v = 0, 1, 0.1 do
    engrave(v,
 	   translate(320, 320) *
